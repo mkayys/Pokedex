7f591f13504f4d400ddd449377657760793585a4
Michelle Kim <x1michelleskim@gmail.com>
2019-09-02 12:25:30 -0700

    Add itemsReducer and uiReducer

--- frontend/reducers/pokemon_reducer.js
+++ frontend/reducers/pokemon_reducer.js
@@ -12,7 +12,7 @@ const pokemonReducer = (state = {}, action) => {
             nextState = Object.assign({}, state, action.pokemon);
             return nextState;
         case RECEIVE_SINGLE_POKEMON:
-            nextState = Object.assign({}, state, { [action.pokemon.id]: action.pokemon })
+            nextState = Object.assign({}, state, { [action.payload.pokemon.id]: action.payload.pokemon })
         default:
             return state;
     }
