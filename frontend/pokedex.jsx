c055ab3bfc9116907a9af31ddb16e8cd8853788a
Michelle Kim <x1michelleskim@gmail.com>
2019-09-02 10:33:06 -0700

    Create a pokemon index container to pass pokemon slice of state and requestAllPokemon thunk action to props

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import configureStore from './store/store';
+import Root from './components/root';
 
 import { fetchAllPokemon } from './util/api_util';
 import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
@@ -22,5 +23,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.selectAllPokemon = selectAllPokemon;
 
-    ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+    ReactDOM.render(<Root store={store}/>, rootEl);
 });
