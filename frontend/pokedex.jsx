8fc7370607e95792ee1f7ac524fe198c3847dfd0
Michelle Kim <x1michelleskim@gmail.com>
2019-09-02 09:50:42 -0700

    Create api util to fetchAllPokemon and an action creator to receiveAllPokemon

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { fetchAllPokemon } from './util/api_util';
+import { receiveAllPokemon } from './actions/pokemon_actions';
+
 document.addEventListener('DOMContentLoaded', () => {
     const rootEl = document.getElementById('root');
     //app/views/static_pages/root.html
+
+
+    //testing:
+
+    window.fetchAllPokemon = fetchAllPokemon;
+    window.receiveAllPokemon = receiveAllPokemon;
+
     ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
