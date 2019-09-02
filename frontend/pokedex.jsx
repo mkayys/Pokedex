6535b319dd806f7cd2b78c3ddec607ee2aad5b58
Michelle Kim <x1michelleskim@gmail.com>
2019-09-02 11:00:30 -0700

    Import HashRouter and Router to wrap components in root file

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { HashRouter, Route } from 'react-router-dom';
 
 import configureStore from './store/store';
 import Root from './components/root';
 
-import { fetchAllPokemon } from './util/api_util';
-import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
-import { selectAllPokemon } from './reducers/selectors';
+//testing imports:
+// import { fetchAllPokemon } from './util/api_util';
+// import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
+// import { selectAllPokemon } from './reducers/selectors';
 
 document.addEventListener('DOMContentLoaded', () => {
     const store = configureStore();
@@ -14,14 +16,16 @@ document.addEventListener('DOMContentLoaded', () => {
     //app/views/static_pages/root.html
 
     //testing:
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
+    // window.getState = store.getState;
+    // window.dispatch = store.dispatch;
 
-    window.requestAllPokemon = requestAllPokemon;
-    window.fetchAllPokemon = fetchAllPokemon;
-    window.receiveAllPokemon = receiveAllPokemon;
+    // window.requestAllPokemon = requestAllPokemon;
+    // window.fetchAllPokemon = fetchAllPokemon;
+    // window.receiveAllPokemon = receiveAllPokemon;
 
-    window.selectAllPokemon = selectAllPokemon;
+    // window.selectAllPokemon = selectAllPokemon;
+
+    //testing end
 
     ReactDOM.render(<Root store={store}/>, rootEl);
 });
