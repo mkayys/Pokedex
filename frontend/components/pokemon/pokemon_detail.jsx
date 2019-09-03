8b1bfe3ead93c63087f3708305b7f0491f3ba3e6
Michelle Kim <x1michelleskim@gmail.com>
2019-09-03 10:48:25 -0700

    Add lodash and update reducer to fix state being overwritten on refresh

--- frontend/components/pokemon/pokemon_detail.jsx
+++ frontend/components/pokemon/pokemon_detail.jsx
@@ -9,11 +9,11 @@ class PokemonDetail extends React.Component {
         this.props.requestSinglePokemon(this.props.match.params.pokemonId);
     }
 
-    // componentDidUpdate(prevProps) {
-    //     if (prevProps.match.params.pokemonId !== this.props.match.params.pokemonId) {
-    //         this.props.requestSinglePokemon(this.props.match.params.pokemonId);
-    //     }
-    // }
+    componentDidUpdate(prevProps) {
+        if (prevProps.match.params.pokemonId !== this.props.match.params.pokemonId) {
+            this.props.requestSinglePokemon(this.props.match.params.pokemonId);
+        }
+    }
 
 
     render() {
@@ -22,19 +22,18 @@ class PokemonDetail extends React.Component {
         return (
             <div className='pokemon-details'>
                 <img className='pokemon-main-img' src={this.props.pokemon.image_url} alt={this.props.pokemon.name} />
-
                 <div className='pokemon-info'>
-                    <div>{this.props.pokemon.name}</div>
-                    <div>
+                    <h2 className='pokemon-name'>{this.props.pokemon.name}</h2>
+                    <div className='pokemon-type'>
                         Type: {this.props.pokemon.poke_type}
                     </div>
-                    <div>
+                    <div className='pokemon-attack'>
                         Attack: {this.props.pokemon.attack}
                     </div>
-                    <div>
+                    <div className='pokemon-defense'>
                         Defense: {this.props.pokemon.defense}
                     </div>
-                    <div>
+                    <div className='pokemon-moves'>
                         Moves: {this.props.pokemon.moves}
                     </div>
                 </div>
--- frontend/reducers/pokemon_reducer.js
+++ frontend/reducers/pokemon_reducer.js
@@ -3,16 +3,23 @@ import {
     RECEIVE_SINGLE_POKEMON 
 } from '../actions/pokemon_actions';
 
+import { merge } from 'lodash';
+
 const pokemonReducer = (state = {}, action) => {
     Object.freeze(state);
-    let nextState;
+    let nextState = {};
 
     switch (action.type) {
         case RECEIVE_ALL_POKEMON:
-            nextState = Object.assign({}, state, action.pokemon);
+            // debugger
+            Object.keys(action.pokemon).map(pokeId => {
+                let old = state[pokeId] === undefined ? {} : state[pokeId]
+                nextState[pokeId] = Object.assign(old, action.pokemon[pokeId])
+            })
+            // nextState = merge({}, state, action.pokemon);
             return nextState;
         case RECEIVE_SINGLE_POKEMON:
-            nextState = Object.assign({}, state, { [action.payload.pokemon.id]: action.payload.pokemon });
+            nextState = merge({}, state, { [action.payload.pokemon.id]: action.payload.pokemon });
             return nextState;
         default:
             return state;
